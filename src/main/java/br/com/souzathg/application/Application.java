c0a5ab852a6443a8ae8adfbbcfc15a946b6d36da
Thiago Costa de Souza <thgsouza@outlook.com>
2022-11-25 15:22:46 -0300

    Add db configuration

--- src/main/java/br/com/souzathg/application/Application.java
+++ src/main/java/br/com/souzathg/application/Application.java
@@ -1,6 +1,6 @@
-package br.com.souzathg;
+package br.com.souzathg.application;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
