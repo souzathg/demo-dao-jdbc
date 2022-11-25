9104b2f225737fc5269f3aff8deb800301e92a6d
Thiago Costa de Souza <thgsouza@outlook.com>
2022-11-25 16:53:43 -0300

    Reusing instances

--- src/main/java/br/com/souzathg/application/Application.java
+++ src/main/java/br/com/souzathg/application/Application.java
@@ -8,6 +8,8 @@ public class Application {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("==== Test 1: Seller Test findById ====");
+
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
