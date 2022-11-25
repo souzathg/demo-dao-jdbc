df7b112652c63a30b7a3ddfefc12642c26a75e42
Thiago Costa de Souza <thgsouza@outlook.com>
2022-11-25 15:28:04 -0300

    Add Department entity class

--- src/main/java/br/com/souzathg/application/Application.java
+++ src/main/java/br/com/souzathg/application/Application.java
@@ -1,7 +1,10 @@
 package br.com.souzathg.application;
 
+import br.com.souzathg.model.entities.Department;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
