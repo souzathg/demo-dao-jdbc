b112dd0615c6bb329e0bc1898c3f0a0de737a4f6
Thiago Costa de Souza <thgsouza@outlook.com>
2022-11-25 15:33:15 -0300

    Add Seller class

--- src/main/java/br/com/souzathg/application/Application.java
+++ src/main/java/br/com/souzathg/application/Application.java
@@ -1,10 +1,14 @@
 package br.com.souzathg.application;
 
 import br.com.souzathg.model.entities.Department;
+import br.com.souzathg.model.entities.Seller;
+
+import java.util.Date;
 
 public class Application {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+        System.out.println(seller);
     }
 }
